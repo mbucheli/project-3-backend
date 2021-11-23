01993dadc7f5d43d6e73be57060b06e7ba51542d
Mavi Bucheli <mavibucheli@gmail.com>
2021-11-22 20:43:13 -0500

    changed schema

--- models/task.js
+++ models/task.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TaskSchema = new mongoose.Schema({
     medicine: {type: String, required: true},
     details: String,
-    quantity: Number,
+    quantity: String,
     date: { type: Date, default: Date.now }
 },
 
